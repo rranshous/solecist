617ca30c146958c0f221aeea7d81bf78c6fbe114
Robby Ranshous <rranshous@gmail.com>
2015-01-31 19:19:15 -0500

    closer, not all trans working

--- app.rb
+++ app.rb
@@ -22,6 +22,7 @@ class MemoryStore < Hash
     @data[key] << [Time.now.to_i, view_version, data]
   end
   def read key, view_version
+    @data[key].to_a rescue 'WTF'
     @data[key].to_a.map do |(_, source_view_version, data)|
       { view_version: source_view_version, data: data }
     end
@@ -33,7 +34,9 @@ class Munger
     @views = SortedSet.new
   end
   def add_view view
-    @views << [view.version, view]
+    unless @views.to_a.map{|ver,_|ver}.include? view.version
+      @views << [view.version, view]
+    end
   end
   def view_lookup
     Hash[@views.to_a]
@@ -44,17 +47,24 @@ class Munger
     # than munge the last view down to the target
 
     # munge the slices up to the highest view we have
+    @views.to_a rescue 'WTF'
     top_view = @views.to_a.last.last
+    puts "TO HIGHEST"
     top_view_data = _munge(slices, top_view)
+    puts "TOP VIEW DATA: #{top_view_data}"
 
     # munge that data to the target view
+    puts "TO TARGET #{target_view.version}"
     target_view_data = _munge([{ view_version: top_view.version,
                                  data: top_view_data }],
                              target_view)
+    puts "TARGET DATA: #{target_view_data}"
 
     return target_view_data
   end
   def _munge slices, target_view
+
+    puts "MUNGE to #{target_view.version}"
 
     # go through each of the slices transforming their
     # data into the target view's version
@@ -64,9 +74,11 @@ class Munger
     # TODO: guarentee order?
     slices.each do |slice|
 
+
       # better to do no work, skip to the end
       # TODO: this short circuit is ugly
       slice_view_version = slice[:view_version]
+      puts " SLICE VIEW VERSION: #{slice_view_version}"
       if slice_view_version == target_view.version
         final_data.merge! slice[:data]
         next
@@ -81,6 +93,7 @@ class Munger
       direction = slice_view_version < target_view.version ? :UP : :DOWN
       views = @views.to_a.dup
       views.reverse! if direction == :DOWN
+      puts " DIRECTION: #{direction}"
 
       # figure out as we walk the chain where we should start and
       # where we should stop our transformations
@@ -88,27 +101,32 @@ class Munger
         start_version = slice_view_version+1
         end_version = target_view.version
       elsif direction == :DOWN
-        start_version = target_view.version
-        end_version = slice_view_version+1
+        start_version = target_view.version+1
+        end_version = slice_view_version
       end
+      puts " START #{start_version} :: END #{end_version}"
       munge_views = views.select do |view_version, _|
         view_version <= end_version && view_version >= start_version
       end
+      puts " MUNGE VIEWS: #{munge_views.map{|v|v.first}}"
 
       # munge our slice data throuh each of the view transformations
-      new_slice_data = {}
       munge_views.each do |(_,munge_view)|
-        new_slice_data.merge(
-          munge_view.transform(direction, slice_data, final_data))
+        slice_data = munge_view.transform(direction, slice_data, final_data)
       end
 
       # merge this slice's munged data into the final entities data
       final_data.merge! slice_data
     end
+
+    # filter the data down to the final view's schema
+    filtered_final_data = target_view.filter final_data
+
+
     # we should have now gone through all the slices
     # merging their data (transformed into the target view's format)
     # into the final data. fin
-    return final_data
+    return filtered_final_data
   end
 end
 
@@ -119,18 +137,31 @@ class View
   def version
     @schema[:VERSION]
   end
+  def filter data
+    Hash[ data.to_a.select{|(k,_)| @schema.include?(k)} ]
+  end
   def transform direction, data, reference_data
     # it seems i'll need to transform the reference data
     # up to my view so that i can reference it in terms i understand
     new_data = {}
     @schema.each do |field, value|
       next if field == :VERSION
-      if value.is_a?(Hash)
+      current_data = reference_data.merge data
+      # if we are going up or down, inherit means copy
+      if value == :INHERIT
+        new_data[field] = current_data[field]
+      # if we are going up new means nil out
+      elsif value == :NEW && direction == :UP
+        new_data[field] = nil
+      # if we are going down new means copy
+      elsif value == :NEW && direction == :DOWN
+        new_data[field] = current_data[field]
+      # hash means transformation
+      elsif value.is_a?(Hash)
         dir_values = value[direction]
         target_field = dir_values[:target] || field
         sources_fields = dir_values[:source]
         transformer = dir_values[:transformer]
-        current_data = reference_data.merge data
         new_value = transformer.call(*sources_fields.map{|f|current_data[f]})
         new_data[target_field] = new_value
       end
--- app_spec.rb
+++ app_spec.rb
@@ -5,14 +5,16 @@ describe 'app' do
 
   let(:store) { MemoryStore.new }
   let(:solecist) { Solecist.new(store) }
-
-  it 'can write using diff views and read newesting using diff views' do
-    data_view_v1 = {
+  let(:entity_key) { 'user::1' }
+  let(:data_view_v1) do
+    {
       VERSION: 1,
       name: :NEW,
       phone: :NEW
     }
-    data_view_v2 = { 
+  end
+  let(:data_view_v2) do
+    { 
       VERSION: 2,
       fname: {
         UP: {
@@ -38,40 +40,66 @@ describe 'app' do
       },
       phone: :INHERIT
     }
-    data_view_v3 = {
+  end
+  let(:data_view_v3) do
+    {
       VERSION: 3,
       fname: :INHERIT, lname: :INHERIT, phone: :INHERIT,
       contact: :NEW
     }
-    solecist.write(
-      'user::1',
-      data_view_v1,
-      { name: 'Robby Ranchous', phone: '1231233' }
-    )
-    solecist.write(
-      'user::1',
-      data_view_v2,
-      { lname: 'Ranshous' }
-    )
-    solecist.write(
-      'user::1',
-      data_view_v3,
-      { contact: 'Lizz' }
-    )
-    expect( solecist.read(
-      'user::1',
-      data_view_v1
-    )).to eq({ name: 'Robby Ranshous',
-               phone: '1231233' })
-    expect( solecist.read(
-      'user::1',
-      data_view_v2
-    )).to eq({ fname: 'Robby', lname: 'Ranshous',
-               phone: '1231233' })
-    expect( solecist.read(
-      'user::1',
-      data_view_v3
-    )).to eq({ fname: 'Robby', lname: 'Ranshous',
-               phone: '1231233', contact: 'Lizz' })
+  end
+
+  it 'can read and write using same version' do
+    solecist.write(entity_key, data_view_v1,
+      { name: 'Robby Ranchous', phone: '1231233' })
+    expect(solecist.read(entity_key, data_view_v1))
+      .to eq({ name: 'Robby Ranchous', phone: '1231233' })
+  end
+
+  context 'three view versions, three writes' do
+    before(:each) do
+      solecist.write(entity_key, data_view_v1,
+        { name: 'Robby Ranchous', phone: '1231233' })
+      solecist.write(entity_key, data_view_v2, { lname: 'Ranshous' })
+      solecist.write(entity_key, data_view_v3, { contact: 'Lizz' })
+    end
+    it 'can read back using first version' do
+      expect(solecist.read(entity_key, data_view_v1))
+        .to eq({ name: 'Robby Ranshous', phone: '1231233' })
+    end
+    it 'can read back using second version (transformations)' do
+      expect(solecist.read(entity_key, data_view_v2))
+        .to eq({ fname: 'Robby', lname: 'Ranshous', phone: '1231233' })
+    end
+    it 'can read back using third version' do
+      expect(solecist.read(entity_key, data_view_v3))
+        .to eq({ fname: 'Robby', lname: 'Ranshous',
+                 phone: '1231233', contact: 'Lizz' })
+    end
+  end
+
+  context 'three view versions, three in order, three out of order writes' do
+    before(:each) do
+      solecist.write(entity_key, data_view_v1,
+        { name: 'Robby Ranchous', phone: '1231233' })
+      solecist.write(entity_key, data_view_v2, { lname: 'Ranshous' })
+      solecist.write(entity_key, data_view_v3, { contact: 'Lizz' })
+      solecist.write(entity_key, data_view_v2, { fname: 'Bobby' })
+      solecist.write(entity_key, data_view_v1, { name: 'Bobby Knight' })
+      #solecist.write(entity_key, data_view_v3, { contact: 'Lizz M' })
+    end
+    it 'can read back using first version' do
+      expect(solecist.read(entity_key, data_view_v1))
+        .to eq({ name: 'Bobby Knight', phone: '1231233' })
+    end
+    it 'can read back using second version (transformations)' do
+      expect(solecist.read(entity_key, data_view_v2))
+        .to eq({ fname: 'Bobby', lname: 'Knight', phone: '1231233' })
+    end
+    it 'can read back using third version' do
+      expect(solecist.read(entity_key, data_view_v3))
+        .to eq({ fname: 'Bobby', lname: 'Knight',
+                 phone: '1231233', contact: 'Lizz M' })
+    end
   end
 end
